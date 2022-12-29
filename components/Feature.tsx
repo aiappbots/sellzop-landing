5e50774044e0aadf6c88052bfb12e5ac87d426ce
“Robert <digitalmediaflow@gmail.com>
2022-12-29 01:25:08 -0500

    static pages setup with tailwind ui

---
+++ components/Feature.tsx
@@ -0,0 +1,80 @@
+import {
+	BoltIcon,
+	ChatBubbleLeftRightIcon,
+	DevicePhoneMobileIcon,
+	DocumentTextIcon,
+	GlobeAltIcon,
+	LifebuoyIcon,
+	ScaleIcon,
+} from "@heroicons/react/24/outline";
+
+const features = [
+	{
+		name: "AI Powered Product Editor",
+		description:
+			"Creates compelling product descriptions to help businesses showcase their offerings and drive sales.",
+		icon: DocumentTextIcon,
+	},
+	{
+		name: "Real Time Product Chat",
+		description:
+			"A real-time chat option on individual products to help you increase your stores customer service and engagement.",
+		icon: ChatBubbleLeftRightIcon,
+	},
+	{
+		name: "Built-in SEO and social media optimization tools.",
+		description:
+			"Which make it easier for potential customers to find your store and products online. We also have an AI product editor that creates compelling product descriptions to help you showcase your offerings and drive sales.",
+		icon: BoltIcon,
+	},
+	{
+		name: "Multi-Language Support",
+		description:
+			"Sell in any laguage,  increase sales to a greater audience all over the world.",
+		icon: LifebuoyIcon,
+	},
+];
+
+export default function Feature() {
+	return (
+		<div className='bg-white py-24 sm:py-32 lg:py-40'>
+			<div className='mx-auto max-w-7xl px-6 lg:px-8'>
+				<div className='sm:text-center'>
+					<h2 className='text-lg font-semibold leading-8 text-indigo-600'>
+						Just some of our features
+					</h2>
+					<p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+						An easier way to sell products and cars online.
+					</p>
+					<p className='mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600'>
+						Our e-commerce platform is a comprehensive online store builder
+						marketing tool that saves you time to easily build and scale your
+						on-line business.
+					</p>
+				</div>
+
+				<div className='mt-20 max-w-lg sm:mx-auto md:max-w-none'>
+					<div className='grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16'>
+						{features.map((feature) => (
+							<div
+								key={feature.name}
+								className='relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row'>
+								<div className='flex h-12 w-12 items-center justify-center rounded-xl bg-gray-800 text-white sm:shrink-0'>
+									<feature.icon className='h-8 w-8' aria-hidden='true' />
+								</div>
+								<div className='sm:min-w-0 sm:flex-1'>
+									<p className='text-lg font-semibold leading-8 text-gray-900'>
+										{feature.name}
+									</p>
+									<p className='mt-2 text-base leading-7 text-gray-600'>
+										{feature.description}
+									</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ components/Header.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+import logo from "../public/new_logo.webp";
+
+const navigation: any = [
+	// { name: "About", href: "#" },
+	// { name: "Features", href: "#" },
+	// // { name: "Docs", href: "#" },
+	// { name: "Company", href: "#" },
+];
+
+export default function Header() {
+	return (
+		<header className='bg-gray-800 m-0'>
+			<nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8' aria-label='Top'>
+				<div className='flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none'>
+					<div className='flex items-center'>
+						<a href='#'>
+							<span className='sr-only'>SellZop</span>
+							<Image className='h-6' width='100' src={logo} alt='logo' />
+						</a>
+						<div className='ml-10 hidden space-x-8 lg:block'>
+							{navigation.map((link: any) => (
+								<a
+									key={link.name}
+									href={link.href}
+									className='text-base font-medium text-white hover:text-indigo-50'>
+									{link.name}
+								</a>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className='flex flex-wrap justify-center space-x-6 py-4 lg:hidden'>
+					{navigation.map((link: any) => (
+						<a
+							key={link.name}
+							href={link.href}
+							className='text-base font-medium text-white hover:text-indigo-50'>
+							{link.name}
+						</a>
+					))}
+				</div>
+			</nav>
+		</header>
+	);
+}
