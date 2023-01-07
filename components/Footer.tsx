31cc4bc4eda6d052dc2836bd772ba08b8a8ff5e4
“Robert <digitalmediaflow@gmail.com>
2023-01-07 15:01:06 -0500

    add Inc. text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -101,7 +101,7 @@ export default function Footer() {
 					))}
 				</div>
 				<p className='mt-8 text-center text-base text-gray-400'>
-					&copy; 2023 SellZop, All rights reserved.
+					&copy; 2023 SellZop Inc. All rights reserved.
 				</p>
 			</div>
 		</footer>
